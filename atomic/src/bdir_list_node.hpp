49d83b78a34dc3f994c4454e89b6323d6909e6ac
hubert97 <hubert.kowalski10@gmail.com>
2021-01-17 20:11:40 +0100

    naprawiony wyciek pamieci, riterator dodano, dodano rback, rend, popfront, popback

--- atomic/src/bdir_list_node.hpp
+++ atomic/src/bdir_list_node.hpp
@@ -6,11 +6,13 @@ template <class T>
 
 class list_node {
 public:
-    list_node(T new_container) { container = new_container; }
+    list_node(T new_container) { container = new_container; terminator=0;}
+    list_node() { terminator=1;}
     T get() { return container; }
     std::shared_ptr<list_node<T>> prev_elem;
     std::weak_ptr<list_node<T>> next_elem;
     T container;
+    int terminator;
 };
 }  // namespace atomic_list
 
--- atomic/src/bidir_list_interface.hpp
+++ atomic/src/bidir_list_interface.hpp
@@ -11,6 +11,7 @@ template <class T>
 
 class Atomic_Blist : public boost::interprocess::interprocess_mutex {
 public:
+//////////////// iterator ////////////////////////////
     class iterator : public std::iterator<std::bidirectional_iterator_tag, T, T> {
     public:
         explicit iterator(std::shared_ptr<atomic_list::list_node<T>> node) : _node(node) {}
@@ -27,7 +28,13 @@ public:
             }
             return curr_it;
         }
-        iterator& operator--();
+        iterator& operator--()
+        {
+            if (_node->next_elem.lock()) {
+                _node = _node->next_elem.lock();
+            }
+            return *this;
+        }
         iterator operator--(int);
 
         T& operator*() const { return _node->container; }
@@ -38,82 +45,147 @@ public:
         std::shared_ptr<atomic_list::list_node<T>> _node;
     };
 
-    Atomic_Blist() { size = 0; }
+//////////////// reverse iterator ////////////////////////////
+
+        class riterator : public std::iterator<std::bidirectional_iterator_tag, T, T> {
+    public:
+        explicit riterator(std::shared_ptr<atomic_list::list_node<T>> node) : _node(node) {}
+        riterator& operator--() {
+            if (_node->prev_elem) {
+                _node = _node->prev_elem;
+            }
+            return *this;
+        }
+        riterator& operator++() {
+            if (_node->next_elem.lock()) {
+                _node = _node->next_elem.lock();
+            }
+            return *this;
+        }
+        riterator operator--(int)
+        {
+            auto curr_it = riterator(_node);
+            if (_node->prev_elem) {
+                _node = _node->prev_elem;
+            }
+            return curr_it;
+
+        }
+        riterator operator++(int){
+            auto curr_it = riterator(_node);
+            if (_node->next_elem.lock()) {
+                _node = _node->next_elem.lock();
+            }
+            return curr_it;
+        }
+
+        T& operator*() const { return _node->container; }
+        bool operator==(riterator other) const { return (_node.get() == other._node.get()); }
+        bool operator!=(riterator other) const { return !(*this == other); }
+
+    private:
+        std::shared_ptr<atomic_list::list_node<T>> _node;
+    };
+//////////////// konstruktor i destruktor ////////////////////////////
+    Atomic_Blist() { size = 0; 
+        past_last.reset();
+        past_first.reset();
+        past_last= std::make_shared<list_node<T>>();
+        past_first= std::make_shared<list_node<T>>();
+    }
+     ~Atomic_Blist() { 
+        last_elem.reset();
+        first_elem.reset();
+        past_last.reset();
+        past_first.reset();
+      }
+
+//////////////////// POP BACK////////////////////////////
 
     T pop_back() {
         this->lock();
         T tmp = last_elem.lock()->get();
 
         if (size > 1) {
-            last_elem = last_elem.lock()->next_elem;
-            last_elem.lock()->prev_elem = first_elem;
-
+            last_elem = last_elem.lock()->next_elem.lock();
+            last_elem.lock()->prev_elem = past_last;
             size--;
         } else if (size <= 1) {
             last_elem.reset();
             first_elem.reset();
+            size=0;
         }
         this->unlock();
         return tmp;
     }
+
+/////////////////// POP FRONT ////////////////////////////
+
+    
     T pop_front() {
         this->lock();
         T tmp = first_elem->get();
         if (size > 1) {
             first_elem = first_elem->prev_elem;
-            last_elem.lock()->next_elem = first_elem;
-            first_elem->next_elem = last_elem;
-
+            first_elem->next_elem = past_first;
             size--;
         } else if (size <= 1) {
             last_elem.reset();
             first_elem.reset();
+            size=0;
         }
+
+
+
         this->unlock();
         return tmp;
     }
-
+//////////////////// PUSH  BACK////////////////////////////
     void push_back(T new_class) {
         this->lock();
         if (last_elem.lock()) {
             std::shared_ptr<atomic_list::list_node<T>> tmp = std::make_shared<list_node<T>>(new_class);
             last_elem.lock()->prev_elem = tmp;
             tmp->next_elem = last_elem;
-            tmp->prev_elem = first_elem;
             last_elem = tmp;
-            first_elem->next_elem.reset();
 
         } else {
             std::shared_ptr<atomic_list::list_node<T>> tmp = std::make_shared<list_node<T>>(new_class);
-            tmp->next_elem = tmp;
-            tmp->prev_elem = tmp;
             last_elem = tmp;
             first_elem = tmp;
         }
+        first_elem->next_elem=past_first;
+        last_elem.lock()->prev_elem=past_last;
+
         size++;
         this->unlock();
     }
+    //////////////////////// PUSH FRONT////////////////////////////
     void push_front(T new_class) {
         this->lock();
         if (first_elem) {
             std::shared_ptr<atomic_list::list_node<T>> tmp = std::make_shared<list_node<T>>(new_class);
             tmp->prev_elem = first_elem;
             first_elem->next_elem = tmp;
-            tmp->next_elem = last_elem.lock();
             first_elem = tmp;
         } else {
             std::shared_ptr<atomic_list::list_node<T>> tmp = std::make_shared<list_node<T>>(new_class);
-            tmp->next_elem = tmp;
-            tmp->prev_elem = tmp;
             last_elem = tmp;
             first_elem = tmp;
         }
+        first_elem->next_elem=past_first;
+        last_elem.lock()->prev_elem=past_last;
+
         size++;
         this->unlock();
     }
 
+//ITERATORY
+
     iterator begin() { return iterator(first_elem); }
-    iterator end() { return iterator(last_elem.lock()); }
+    riterator rbegin() {return riterator(last_elem.lock());}
+    iterator end() { return iterator(past_last); }
+    riterator rend() { return riterator(past_first); }
 
     void clear() {
         this->lock();
@@ -122,9 +194,15 @@ public:
         this->unlock();
     }
 
+
+
 private:
     std::shared_ptr<atomic_list::list_node<T>> first_elem;
     std::weak_ptr<atomic_list::list_node<T>> last_elem;
+    
+    std::shared_ptr<atomic_list::list_node<T>> past_last;
+    std::shared_ptr<atomic_list::list_node<T>> past_first;
+
 
     int size;
     int elem_iterator;
